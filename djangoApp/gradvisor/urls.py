b8dad51ae28cf4a8b946ea45b1644ca1fdde538f
ShwetimaSakshi <shwetimasakshi@tamu.edu>
2024-04-13 14:35:21 -0500

    Update urls.py

--- djangoApp/gradvisor/urls.py
+++ djangoApp/gradvisor/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 from . import views
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
-    path('', views.applicant_form, name='applicant_form'),
-    path('success/', views.success, name='success'),
+    path('applicant_form/', views.applicant_form, name='applicant_form'),
+    # path('success/', views.success, name='success'),
+    # path('login/', views.login_view, name='login'),
+    path('admin/', admin.site.urls),
+    path('', views.signup_login_view, name='login_signup'),
 ]
