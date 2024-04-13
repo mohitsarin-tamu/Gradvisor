acdfa31dbe888bdd1ec5df81c7aaadb8857093b4
ShwetimaSakshi <shwetimasakshi@tamu.edu>
2024-04-13 18:11:42 -0500

    Update views.py

--- djangoApp/gradvisor/views.py
+++ djangoApp/gradvisor/views.py
@@ -8,6 +8,9 @@ from .models import Applicant
 from django.contrib import messages
 
 def home(request):
+    return render(request, 'home.html') 
+
+def signup_login_view(request):
     signup_form = UserCreationForm()
     login_form = AuthenticationForm()
 
@@ -17,8 +20,7 @@ def home(request):
             if signup_form.is_valid():
                 user = signup_form.save()
                 login(request, user)
-                messages.success(request, "Signup successful. Please login.")
-                return redirect('home')
+                return redirect('applicant_form')
         elif 'login' in request.POST:
             login_form = AuthenticationForm(request, data=request.POST)
             if login_form.is_valid():
@@ -27,9 +29,9 @@ def home(request):
                 user = authenticate(username=username, password=password)
                 if user is not None:
                     login(request, user)
-                    return redirect('home')
+                    return redirect('applicant_form')
 
-    return render(request, 'home.html', {'signup_form': signup_form, 'login_form': login_form})
+    return render(request, 'signup_login.html', {'signup_form': signup_form, 'login_form': login_form})
 
 
 def applicant_form(request):
@@ -49,53 +51,3 @@ def success(request):
 def applicant_list(request):
     applicants = Applicant.objects.all()
     return render(request, 'applicant_list.html', {'applicants': applicants})
-
-# def signup_view(request):
-#     if request.method == 'POST':
-#         form = UserCreationForm(request.POST)
-#         if form.is_valid():
-#             user = form.save()
-#             login(request, user)
-#             return redirect('login')  # Redirect to login page after successful signup
-#     else:
-#         form = UserCreationForm()
-#     return render(request, 'signup.html', {'form': form})
-
-# def login_view(request):
-#     if request.method == 'POST':
-#         form = AuthenticationForm(request, data=request.POST)
-#         if form.is_valid():
-#             user = form.get_user()
-#             login(request, user)
-#             return redirect('home')  # Redirect to home page after successful login
-#     else:
-#         form = AuthenticationForm()
-#     return render(request, 'login.html', {'form': form})
-
-# def logout_view(request):
-#     logout(request)
-#     return redirect('home')  # Redirect to home page after logout
-
-def signup_login_view(request):
-    signup_form = UserCreationForm()
-    login_form = AuthenticationForm()
-
-    if request.method == 'POST':
-        if 'signup' in request.POST:
-            signup_form = UserCreationForm(request.POST)
-            if signup_form.is_valid():
-                user = signup_form.save()
-                login(request, user)
-                messages.success(request, "Signup successful. Please login.") 
-                # return redirect('home')  # Redirect to home page after successful signup
-        elif 'login' in request.POST:
-            login_form = AuthenticationForm(request, data=request.POST)
-            if login_form.is_valid():
-                username = login_form.cleaned_data.get('username')
-                password = login_form.cleaned_data.get('password')
-                user = authenticate(username=username, password=password)
-                if user is not None:
-                    login(request, user)
-                    return redirect('home')  # Redirect to home page after successful login
-
-    return render(request, 'signup_login.html', {'signup_form': signup_form, 'login_form': login_form})
