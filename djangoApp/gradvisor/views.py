08ab37479d937421730fb35c8d34c3c7dc4ef457
Shaunak Joshi <shaunakjoshi1407@gmail.com>
2024-04-27 02:07:10 -0500

    Added user similarity using KNN to our UI (#11)
    * Update Gradvisor
    
    Update Gradvisor
    
    * Update Gradvisor
    
    Update Gradvisor
    
    * Commiting knn model for user-user similarity
    
    * Update Gradvisor
    
    Update Gradvisor
    
    * Commiting knn model for user-user similarity
    
    * Create GradVisor_udpated.ipynb
    
    * Some knn model changes
    
    * Display similar users on the UI
    
    ---------
    
    Co-authored-by: mohitsarin-tamu <mohitsarin26@tamu.edu>

--- djangoApp/gradvisor/views.py
+++ djangoApp/gradvisor/views.py
@@ -10,6 +10,8 @@ from django.contrib.auth import authenticate
 from django.contrib.auth import login, logout
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.shortcuts import render, redirect
+from sklearn.impute import SimpleImputer
+
 # Create your views here.
 from .forms import ApplicantForm
 from .models import Applicant
@@ -19,6 +21,15 @@ from django.contrib.auth.decorators import login_required
 
 # Get the absolute path to the current directory
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+# Define the filename for y_train
+y_train_filename = 'y_train.csv'
+
+# Construct the full path to y_train.csv
+y_train_path = os.path.join(BASE_DIR, y_train_filename)
+
+# Load y_train from the CSV file
+y_train = pd.read_csv(y_train_path)
 
 # Check scikit-learn version
 if sklearn.__version__ != '1.2.2':
@@ -31,6 +42,35 @@ except ValueError as e:
     print(e)
     print("Please retrain the model.")
     ada_model = None
+
+
+try:
+    with open(os.path.join(BASE_DIR, 'knn_model.pkl'), 'rb') as file:
+        knn_model = joblib.load(file)
+except ValueError as e:
+    print(e)
+    print("Please retrain the model.")
+    knn_model = None
+
+
+
+# Mapping from university name to index
+univ_to_index = {
+    'Worcester Polytechnic Institute': 53, 'Wayne State University': 52, 'Virginia Polytechnic Institute and State University': 51,
+    'University of Wisconsin Madison': 50, 'University of Washington': 49, 'University of Utah': 48, 'University of Texas Dallas': 47,
+    'University of Texas Austin': 46, 'University of Texas Arlington': 45, 'University of Southern California': 44, 'University of Pennsylvania': 43,
+    'University of North Carolina Charlotte': 42, 'University of North Carolina Chapel Hill': 41, 'University of Minnesota Twin Cities': 40,
+    'University of Michigan Ann Arbor': 39, 'University of Massachusetts Amherst': 38, 'University of Maryland College Park': 37,
+    'University of Illinois Urbana-Champaign': 36, 'University of Illinois Chicago': 35, 'University of Florida': 34, 'University of Colorado Boulder': 33,
+    'University of Cincinnati': 32, 'University of California Santa Cruz': 31, 'University of California Santa Barbara': 30,
+    'University of California San Diego': 29, 'University of California Los Angeles': 28, 'University of California Irvine': 27,
+    'University of California Davis': 26, 'University of Arizona': 25, 'Texas A and M University College Station': 24, 'Syracuse University': 23,
+    'SUNY Stony Brook': 21, 'SUNY Buffalo': 20, 'Stanford University': 22, 'Rutgers University New Brunswick/Piscataway': 19, 'Purdue University': 18,
+    'Princeton University': 17, 'Ohio State University Columbus': 16, 'Northwestern University': 15, 'Northeastern University': 14,
+    'North Carolina State University': 13, 'New York University': 12, 'New Jersey Institute of Technology': 11, 'Massachusetts Institute of Technology': 10,
+    'Johns Hopkins University': 9, 'Harvard University': 8, 'Georgia Institute of Technology': 7, 'George Mason University': 6, 'Cornell University': 5,
+    'Columbia University': 4, 'Clemson University': 3, 'Carnegie Mellon University': 2, 'California Institute of Technology': 1, 'Arizona State University': 0
+}
 
 def home(request):
     return render(request, 'home.html') 
@@ -103,14 +143,61 @@ def predict_universities(user_data):
 
     return top_five_predictions
 
+# Define a function to preprocess the input data
+def preprocess_input_data(user_data):
+    
+    # Create a DataFrame from the user data
+    df = pd.DataFrame({
+        'researchExp': [user_data['researchExp']],
+        'industryExp': [user_data['industryExp']],
+        'internExp': [user_data['internExp']],
+        'journalPubs': [user_data['journalPubs']],
+        'confPubs': [user_data['confPubs']],
+        'cgpa': [user_data['cgpa']],
+        'gre_score': [user_data['gre_score']],
+        'univName': [user_data['univName']],
+        'univName_encoded': [user_data['univName_encoded']],  # Include 'univName_encoded'
+    })
+
+    # Create an imputer to fill missing values with the mean
+    imputer = SimpleImputer(strategy='mean')
+
+    # Fit the imputer on the input data and transform it
+    df_imputed = pd.DataFrame(imputer.fit_transform(df), columns=df.columns)
+
+    return df_imputed
+
+
+def similarity_users(user_data):
+    if knn_model is None:
+        return None
+
+    # Preprocess the input data to handle NaN values
+    df = preprocess_input_data(user_data)
+
+    # Predict the top similar users using the KNN model
+    distances, indices = knn_model.kneighbors(df, n_neighbors=3)
+        
+    # Initialize a set to store unique similar users
+    unique_similar_users = set()
+
+    # Iterate over the first 3 similar users
+    for distance, idx in zip(distances[0][:5], indices[0][:5]):
+        # Calculate similarity (1 - distance)
+        similarity = 1 - distance
+        # Get the user index and the corresponding target value (user name)
+        user_name = y_train.iloc[idx].iloc[0]
+        unique_similar_users.add(user_name)
+    
+    return list(unique_similar_users)
+
+
 @login_required
 def applicant_form(request):
     username = request.user.username
     user_id = request.user.id
-    # print('here:', username, user_id)
 
     applicant_entry = Applicant.objects.filter(id=user_id).first()
-    # print("Applicant Entry:", applicant_entry)
 
     if request.method == 'POST':
         form = ApplicantForm(request.POST, instance=applicant_entry)
@@ -140,11 +227,23 @@ def applicant_form(request):
 
             # Make predictions
             top_five_predictions = predict_universities(user_data)
+            # Get the top predicted university name
+            top_university_name = top_five_predictions[0][0]
+           
+            # Get the corresponding index from the univ_to_index dictionary
+            univ_index = univ_to_index.get(top_university_name)
+            
+            user_data['univName'] = univ_index
+            user_data['univName_encoded'] = univ_index
+            similar_users = similarity_users(user_data)
+
+            # Get the top five similar universities
             context = {
                 'username': username,
                 'cgpa': cgpa,
                 'gre_scores': gre_score,
                 'predicted_colleges': top_five_predictions,
+                'similarity_users': similar_users,
             }
             return render(request, 'recommendations.html',  context)
     else:
